9b156cd0785f5c4693fee2fbfd7219b766f448b1
Emily Nichols <emnnichols@outlook.com>
2024-05-05 18:04:05 -0700

    Moved Firebase config

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { getFirestore } from 'firebase/firestore';
+import { app } from './firebase';
+
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs();
+
 import Start from './components/Start';
 import Chat from './components/Chat';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -21,8 +29,9 @@ const App = () => {
 
         <Stack.Screen
           name="Chat"
-          component={Chat}
-        />
+        >
+          {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
